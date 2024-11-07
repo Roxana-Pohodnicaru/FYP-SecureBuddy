21b5d8e75199cf5c8d3371b1ec52a44b74ad369d
Roxana-Pohodnicaru <C21446942@mytudublin.ie>
2024-11-07 22:25:21 +0000

    hamburger menu frame working

--- gui.py
+++ gui.py
@@ -11,7 +11,6 @@ root.title("SecureBuddy")
 # screen size
 root.geometry("1980x1200")
 
-
 # text to show
 text = """Welcome to SecureBuddy"""
 # label for text
@@ -20,10 +19,58 @@ label = tk.Label(root, text=text)
 label.pack()
 
 
+# bool to check if the hamburger menu frame is visible
+is_menu_visible = False
+
+
 # function that handles functionality when hamburger button is clicked
-# displays message when clicked
 def hamburger_menu():
-    clicked_message.config(text="Button was clicked")
+    
+    global is_menu_visible
+    
+    # check if menu is already visible
+    if is_menu_visible:
+        
+        hide_menu() 
+    else:
+        display_menu()
+
+
+# function to hide menu when hamburger button is clicked
+def hide_menu():
+    
+    global is_menu_visible
+    
+    # slide frame towards the left, off screen
+    for i in range(0, -1000, -15): 
+        
+        # display menu frame
+        menu_frame.place(x=i, y=0) 
+        
+        # update each frame movement
+        root.update()
+        
+    is_menu_visible = False
+
+
+# function to display menu when hamburger button is clicked
+def display_menu():
+    
+    global is_menu_visible
+    
+    # slide frame to be displayed on screen
+    for i in range(-1000, 0, 15):
+        
+        # display menu frame
+        menu_frame.place(x=i, y=0)
+        
+        # update each frame movement
+        root.update()
+        
+        # make animation smoother
+        root.after(10)
+        
+    is_menu_visible = True
 
 
 # hamburger menu icon
@@ -31,11 +78,15 @@ hamburger_button = tk.Button(root, text="☰", font=("Arial", 20), command=hambu
 # top left corner
 hamburger_button.place(x=10, y=10)
 
-# show if hamburger button is clicked
-clicked_message = tk.Label(root, text="", font=("Arial", 14))
-clicked_message.pack()
+# create side menu for hamburger button
+# cover 3/4 of the screen
+menu_frame = tk.Frame(root, bg="lightgray", width=800, height=1200)
 
+# when program starts, position hamburger menu off screen
+menu_frame.place(x=-1000, y=0) 
 
+# hamburger button always shows - might change this to an 'x' / 'esc' functionality
+hamburger_button.lift()
 
-# main
+# main loop
 root.mainloop()
