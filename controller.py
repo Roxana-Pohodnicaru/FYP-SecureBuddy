bc27a44a8e34ca00ed33ae8899ef769ffafb6012
Roxana-Pohodnicaru <C21446942@mytudublin.ie>
2025-02-26 21:59:51 +0000

    macros in office files and zip bombs vulns detection implemented

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 import os
+import zipfile
 from datetime import datetime
 from database import DatabaseManager
+from oletools.olevba import VBA_Parser
 
 
 class Controller:
@@ -25,12 +27,14 @@ class Controller:
         
         # scan file to determine its status, risk level, reason, and category
         status, risk_level, reason_for_flag, risk_category = self.scan_file(file_path)
-
+        
         # save metadata to db
         scanned_file_id = self.db_manager.add_scanned_file(
             
             file_name, scan_date, status, risk_level
         )
+
+        print(f"Scanned file ID returned: {scanned_file_id}")
 
         # save scan details to db
         self.db_manager.add_scan_detail(
@@ -52,7 +56,6 @@ class Controller:
             
             prevention_tips="Avoid downloading files from untrusted sources"
         )
-
 
         # logging for debugging
         print(f"File '{file_name}' processed successfully with ID: {scanned_file_id}")
@@ -123,10 +126,13 @@ class Controller:
     def executable_file_checker(self, file_path):
         
         # get file extension from path
+        file_name = os.path.basename(file_path).strip()
+        
+        # split file name into name and extension
+        _, file_extension = os.path.splitext(file_name)
+        
         # gets the last item of filepath and converts to lowercase
-        file_name = os.path.basename(file_path).strip()  # Strip any leading/trailing whitespace
-        _, file_extension = os.path.splitext(file_name)  # Split the file name and extension
-        file_extension = file_extension.lower()  # Convert to lowercase for consistency
+        file_extension = file_extension.lower()
 
         # list of executable file extensions
         executable_extensions = [
@@ -154,51 +160,241 @@ class Controller:
         return None
 
 
+    # method to check if file type is of microsoft office type (doc, xml etc)
+    def office_file_checker(self, file_path):
+        
+        # get file extension from path 
+        file_name = os.path.basename(file_path).strip()
+        
+        # split file name into name and extension
+        _, file_extension = os.path.splitext(file_name)
+        
+        # convert file extension to lowercase
+        file_extension = file_extension.lower()
+        
+        # debugging
+        print(f"Processing file: {file_name}, Extension: {file_extension}")
+        
+        
+        # list of file extensions for microsoft office files
+        office_extensions = [
+            
+            '.doc', '.docm', '.docx', '.dot', '.odt', '.pdf', '.xml', '.xps', '.csv', '.dbf', '.ods', '.xla', '.xlam', '.xls', '.xlsb', '.xlsm', '.xlsx', '.xlt', '.xltm', '.xlw', '.xml', '.xps', '.potm', '.potx', '.ppa', '.ppam', '.ppsx', '.ppt', '.pptm', '.pptx', '.xps' 
+            
+        ]
+        
+        # check if file extension is in office list
+        if file_extension in office_extensions:
+            
+            # if true calls analyse macro function to check for macros
+            return self.analyse_macro(file_path)
+        
+        
+    # method to analyse a file for malicious macros
+    def analyse_macro(self, file_path):
+        
+        try:
+            # initialise VBA parser
+            vba_parser = VBA_Parser(file_path)
+            
+            # list of suspicious keywords used in malicious macros
+            suspicious_content = [
+                       
+                        # common malicious script commands
+                        "Shell", "CreateObject", "Execute", "Run", "Open", "Close", "Kill", "Terminate", "Delete", "Format", "WScript.Shell", "AppActivate", "Application.Run", "AutoOpen", "AutoClose", "Document_Open", "Workbook_Open", "Workbook_BeforeClose",
+
+                        # file manipulation
+                        # system commmands
+                        "FileSystemObject", "Write", "Read", "CopyFile", "DeleteFile", "MoveFile", "GetSpecialFolder", "CreateFolder", "DeleteFolder",
+
+                        # network commands
+                        "XMLHTTP", "WinHttpRequest", "Send", "Post", "Get", "UploadFile", "DownloadFile", "ADODB.Stream",
+
+                        # process commands
+                        "ShellExecute", "CreateProcess", "WinExec", "LoadLibrary", "GetProcAddress", "VirtualAlloc", "VirtualProtect",
+
+                        # registry manipulation commands
+                        "RegWrite", "RegRead", "RegDelete", "HKLM", "HKCU", "HKEY_LOCAL_MACHINE", "HKEY_CURRENT_USER",
+
+                        # powershell commands
+                        "PowerShell", "Invoke-Expression", "Set-ExecutionPolicy", "Bypass", "Start-Process", "Invoke-WebRequest",
+                        
+                        # windows management instrumentation commands
+                        "WMIService", "ExecQuery", "Win32_Process", "GetObject", "ExecMethod", "SpawnInstance",
+
+                        # encoding 
+                        # obfuscation
+                        "Base64Decode", "Base64Encode", "Chr", "ChrW", "StrReverse", "Xor", "Hex", "Randomize", "Eval",
+
+                        # file paths
+                        # environment variables
+                        "C:\\Windows", "C:\\System32", "%TEMP%", "%APPDATA%", "%PROGRAMDATA%", "%USERPROFILE%", "%SYSTEMROOT%",
+
+                        # exploits
+                        "HeapAlloc", "HeapFree", "ROP", "BufferOverflow", "UseAfterFree", "FormatString"
+                        
+            ]
+            
+            # check if file contains VBA macros
+            if vba_parser.detect_vba_macros():
+                
+                print("Macros detected")
+                
+                # extract macros from file
+                for macro_tuple in vba_parser.extract_macros():
+                    
+                    # debugging
+                    print(f"Extracted Macro Tuple: {macro_tuple}")
+                
+                    # iterate through each element in macro tuple
+                    for element in macro_tuple:
+                        
+                        # skip non string elements
+                        if not isinstance(element, str):
+                            
+                            continue
+                    
+                    
+                        # check each element if it contains any malicious commands
+                        for keyword in suspicious_content:
+                            
+                            # case insensitive search
+                            if keyword.lower() in element.lower():
+                                
+                                # suspicious keyword found
+                                # file marked as unsafe
+                                # store in db
+                                return {
+                                    
+                                    "status": "dangerous",
+                                    
+                                    "risk_level": "high",
+                                    
+                                    "reason_for_flag": f"Macros contain suspicious command: {keyword}.",
+                                    
+                                    "risk_category": "Macro-based Threats"
+                                    
+                                }
+
+        except Exception as e:
+            
+            print(f"Error: {e}")
+           
+           
+    # method to analyse ZIP files based on their compression size
+    def detect_zip_bombs(self, file_path, compression_ratio_threshold=1000, max_decompressed_size=10**9):
+        
+        try:
+            # open ZIP file in read mode
+            with zipfile.ZipFile(file_path, 'r') as zip_file:
+                
+                # calculate total compressed size of all files in ZIP
+                total_compressed_size = sum(file.compress_size for file in zip_file.infolist())
+                
+                # calculate total decompressed size of all files in ZIP
+                total_decompressed_size = sum(file.file_size for file in zip_file.infolist())
+
+                # calculate compression ratio
+                if total_compressed_size > 0:
+                    
+                    compression_ratio = total_decompressed_size / total_compressed_size
+                
+                else:
+                    compression_ratio = 0
+
+                # if compression ratio is bigger than ratio threshold
+                if compression_ratio > compression_ratio_threshold:
+                    
+                    # mark file as unsafe
+                    # store in db
+                    return {
+                        
+                        "status": "dangerous",
+                        
+                        "reason_for_flag": f"Compression ratio ({compression_ratio}) exceeds safe threshold.",
+                        
+                        "risk_category": "Zip Bomb"
+                    }
+
+                # if total decompressed size is bigger than 1 GB
+                if total_decompressed_size > max_decompressed_size:
+                    
+                    # mark file as unsafe
+                    # store in db
+                    return {
+                        
+                        "status": "dangerous",
+                        
+                        "reason_for_flag": f"Decompressed size ({total_decompressed_size} bytes) exceeds limit.",
+                        
+                        "risk_category": "Zip Bomb"
+                    }
+
+
+        except Exception as e:
+            
+            # debugging
+            print(f"Error: {e}")
+
+        
+        
+    # method to check if file is ZIP to analyze for ZIP bombs
+    def is_zip_file(self, file_path):
+        
+        # get file extension
+        file_extension = os.path.splitext(file_path)[1].lower()
+        
+        # check if file is ZIP
+        if file_extension == ".zip":
+            
+            # call detect_zip_bombs
+            self.detect_zip_bombs(file_path)
+
+
 
     # method to check file for vulnerabilities
     # temporarily checks file extensions
     # will add more functions which will be called in here
     # placeholder for scanning logic
     def scan_file(self, file_path):
+
+        print(f"Scanning file: {file_path}")
         
-        # start with safe status
+        # default values
         status, risk_level = "safe", "low"
         
         reason_for_flag, risk_category = None, None
 
-        # check for double extensions
-        result = self.double_extension_checker(file_path)
-        
-        # if double extension finds an issue
-        if result:
+        # file checkers
+        checkers = [
             
-            # update data with results
-            status = result["status"]
+            self.double_extension_checker,
             
-            risk_level = result["risk_level"]
+            self.executable_file_checker,
             
-            reason_for_flag = result["reason_for_flag"]
+            self.office_file_checker,
             
-            risk_category = result["risk_category"]
+            self.is_zip_file
+            
+        ]
 
-        
-        
-        # check if file is executable
-        exec_result = self.executable_file_checker(file_path)
-        
-        # if executable finds an issue
-        if exec_result:
+        # iterate through checkers
+        for checker in checkers:
             
-            # update data with results
-            status = exec_result["status"]
+            result = checker(file_path)
             
-            risk_level = exec_result["risk_level"]
-            
-            reason_for_flag = exec_result["reason_for_flag"]
-            
-            risk_category = exec_result["risk_category"]
-
-
+            if result:
+               
+                # update with results
+                status = result["status"]
+                
+                risk_level = result["risk_level"]
+                
+                reason_for_flag = result["reason_for_flag"]
+                
+                risk_category = result["risk_category"]
+                
+                break
 
         # return results
         return status, risk_level, reason_for_flag, risk_category
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,18 +1,1 @@
-# TEMP PAGE FOR PHP MALWARE FOR DEMO PURPOSES
-php_malware_page = tk.Frame(root, bg="white", width=1980, height=1200)
-php_malware_page.place(x=0, y=0)
-
-# php malware header text
-php_malware_header = "PHP Malware"
-php_malware_header_label = tk.Label(php_malware_page, text=php_malware_header, font=("Arial", 20))
-php_malware_header_label.place(x=575, y=50)
-
-# text for php malware
-php_malware_text_1 = "lorem ipsum"
-php_malware_text_1_label = tk.Label(php_malware_page, text=php_malware_text_1, font=("Arial", 20))
-php_malware_text_1_label.place(x=10, y=150)
-
-
-# button for php - TEMP
-more_info_php_malware_button =  tk.Button(scan_report_page, text="more info", font=("Arial, 12"), command=lambda: show_page(php_malware_page))
-more_info_php_malware_button.place(x=1150, y=300, height=50, width=90)
+de
